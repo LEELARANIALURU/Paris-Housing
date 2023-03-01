ea083a4bdabd08ae8f7d5a3dde595400bb28b2fa
unknown <leelaranialuru@gmail.com>
2023-03-02 01:06:44 +0530

    Code update

--- code.py
+++ code.py
@@ -1,21 +1,33 @@
-import numpy as np
 import pandas as pd
 
+#loding the dataset
 data = pd.DataFrame(pd.read_csv("ParisHousing.csv"))
 
-y = data["price"]
-x = data.drop(["price"], axis=1)
+#Correlation of variables
+import matplotlib.pyplot as plt
+import seaborn as sns
+corr = data.corr()
+features = corr.index
+plt.figure(figsize=(20,30))
+ax = sns.heatmap(data[features].corr(),annot=True,cmap='RdYlGn')
+plt.show()
 
+y = data["price"]
+
+# Dropping the attributes which have least correlation with output
+x = data.drop(["price", "cityCode", "hasGuestRoom"], axis=1)
+
+# Splitting dataset
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=2)
 
+# instantiate and fit
 from sklearn.linear_model import LinearRegression
 
 model = LinearRegression()
 
 model.fit(x_train, y_train)
 
-print(model.score(x_test, y_test))
-
-print(model.predict([[80000, 3, 1, 1, 6, 22818, 5, 2, 2020, 0, 1, 2940, 4000, 130,1, 0]]))
+# Prints 99.999957485
+print(model.score(x_test, y_test)*100)
